0c43e6baf9f7ac119975e4ab31662ff1c51371a5
Cabrera Mauricio Hernan <mauricioh_cabrera@hotmail.com>
2018-09-25 18:59:16 -0300

    Intento 2

--- dist/server/config/index.js
+++ dist/server/config/index.js
@@ -8,7 +8,11 @@ var PORT = process.env.PORT || 3000;
 var username = 'hernan';
 var password = 'f0dd46e75d0577e596f7eeb40673dd2e';
 
-var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/platzi-overflow';
+// const mongoUrl = 'mongodb://localhost/platzi-overflow'
+var mongoUrl = 'mongodb://' + username + ':' + password + '@ds113873.mlab.com:13873/platzi-overflow';
+// const mongoUrl = 'mongodb://localhost/platzi-overflow'
+// const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/platzi-overflow'
+
 
 exports.secret = secret;
 exports.mongoUrl = mongoUrl;
--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,10 @@ const PORT = process.env.PORT || 3000
 const username = 'hernan'
 const password = 'f0dd46e75d0577e596f7eeb40673dd2e'
 
-const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/platzi-overflow'
+// const mongoUrl = 'mongodb://localhost/platzi-overflow'
+const mongoUrl = `mongodb://${username}:${password}@ds113873.mlab.com:13873/platzi-overflow`
+// const mongoUrl = 'mongodb://localhost/platzi-overflow'
+// const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/platzi-overflow'
 
 
 export {
